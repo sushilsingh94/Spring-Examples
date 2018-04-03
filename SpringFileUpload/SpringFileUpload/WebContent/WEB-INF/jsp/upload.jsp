3785f6797096dfbdf147df4bdc71609a1d1f68fc
Sushil <sushilsingh94@gmail.com>
2018-04-03 20:53:48 +0530

    Add files via upload

---
+++ SpringFileUpload/SpringFileUpload/WebContent/WEB-INF/jsp/upload.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Spring MVC file upload example</h1>
+
+<form method="POST" action="${pageContext.request.contextPath}/upload" enctype="multipart/form-data">
+    <input type="file" name="file" /><br/>
+    <input type="submit" value="Submit" />
+</form>
+
+<h1>Upload Status</h1>
+<h2>Message : ${message}</h2>
+</body>
+</html>
---
+++ SpringFileUpload/SpringFileUpload/src/main/java/com/learningsolo/spring/FileUploadController.java
@@ -0,0 +1,53 @@
+package com.learningsolo.spring;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class FileUploadController {
+	
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public ModelAndView index() {
+        return new ModelAndView("upload");
+    }
+    
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ModelAndView singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return new ModelAndView("upload","message", "Please select a file to upload");
+        }
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get("C://temp//" + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message", 
+                        "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+        	redirectAttributes.addFlashAttribute("message", 
+                    "Failure occured during upload '" + file.getOriginalFilename() + "'");
+            e.printStackTrace();
+        }
+
+		
+        return new ModelAndView("upload","message", "You successfully uploaded '" + file.getOriginalFilename() + "'");
+    }
+}
